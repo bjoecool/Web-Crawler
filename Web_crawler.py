d40ac446169650f114926700960baea06ec10adf
parthdshah <parthdshah@ufl.edu>
2015-05-10 05:23:04 -0400

    Update Web_crawler.py

--- Web_crawler.py
+++ Web_crawler.py
@@ -10,14 +10,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
-import sys
+from sys import argv, exit
 from datetime import datetime 
 import sched
 
-class getFares():	
+class getFares(): #Class for crawling the website	
 	
-	#self.browser = webdriver.Firefox()
-	#self.browser.get('http://www.united.com/web/en-US/default.aspx?root=1')
 	def __init__(self, src, dest, dptdt, retdt):
 		self.src = src
 		self.dest = dest
@@ -31,40 +29,51 @@ class getFares():
 		self.a_awards = []
 		self.p_fare = []
 		self.d_res = {}	
+		self.browser = webdriver.Firefox()
 	
-	def fillDtls(self):
+	def fillDtls(self): #Function for filling in details
 
 		#user_agent = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) " + "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.57 Safari/537.36")
 
 		#dcap = dict(DesiredCapabilities.PHANTOMJS)
 		#dcap["phantomjs.page.settings.userAgent"] = user_agent
 		
-		browser = webdriver.Firefox()#PhantomJS(desired_capabilities=dcap)
-		browser.get('http://www.united.com/web/en-US/default.aspx?root=1')
-		#time.sleep(3)
-		From = browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_Origin_txtOrigin")]')
+		#self.browser = webdriver.Firefox()#PhantomJS(desired_capabilities=dcap)
+		self.browser.get('http://www.united.com/web/en-US/default.aspx?root=1') #Get response from the website
+
+		#Enter the source value
+		From = self.browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_Origin_txtOrigin")]')
 		From.send_keys(self.src)
 		From.send_keys(Keys.ARROW_DOWN)		
 		
-		To = browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_Destination_txtDestination")]')
+		#Enter the destination value
+		To = self.browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_Destination_txtDestination")]')
 		To.send_keys(self.dest)
 		To.send_keys(Keys.ARROW_DOWN)
 
-		departDt = browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_DepDateTime_Depdate_txtDptDate")]')
+		#Enter the Departure Date value
+		departDt = self.browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_DepDateTime_Depdate_txtDptDate")]')
 		departDt.send_keys(self.dptdt)
 		
-		returnDt = browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_RetDateTime_Retdate_txtRetDate")]')
+		#Enter the Return Date value
+		returnDt = self.browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_RetDateTime_Retdate_txtRetDate")]')
 		returnDt.send_keys(self.retdt)
+		self.srchPrice()
 		
+	def srchPrice(self): #Function for Searching by Price
 		print "Search Criteria: Price"
 		
-		browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_SearchBy_rdosearchby1")]').click()
+		#Select the radio button and click submit
+		self.browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_SearchBy_rdosearchby1")]').click()
 		print "criteria selected"
 		
-		browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_btnSearchFlight")]').click()	
+		self.browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Booking1_btnSearchFlight")]').click()	
 
 		try:
-			dtls = browser.find_elements_by_xpath('//tr[contains(@id, "trSegBlock")]')#/tr[1]/td[contains(@class, "tdSegmentDtl")]/div[1]/b')
+			#Get the flight details from the table and traverse each of the option available
+			#Append the flight details in a list p_flghtlist	
+			
+			dtls = self.browser.find_elements_by_xpath('//tr[contains(@id, "trSegBlock")]')#/tr[1]/td[contains(@class, "tdSegmentDtl")]/div[1]/b')
 			#p_flghtlist = []
 			for i in dtls:
 				flights = i.find_elements_by_xpath('.//td/table/tbody/tr/td[contains(@class, "tdSegmentDtl")]/div[1]')
@@ -72,7 +81,9 @@ class getFares():
 				for j in flights:
 					tmp_list.append(j.find_element_by_tag_name("b").text)
 				self.p_flghtlist.append(tmp_list)
-
+			
+			# Get the departure time and arrival time of the flights and store them in seperate lists 	
+			
 			n = 1
 			for i in dtls:
 				self.p_dpttime.append(i.find_element_by_xpath('.//td/table/tbody/tr[1]/td[contains(@class, "tdDepart")]/div[2]/strong').text)
@@ -88,22 +99,20 @@ class getFares():
 			#print self.p_arrtime
 			#print self.p_arrdt
 			#print self.p_fare
+			self.srchAwards()
+		except NoSuchElementException:
+			print "Wrong Parameter Values"	
 
-		
+	def srchAwards(self): #Function for searching by Award miles
+		try:	
 			print "Search Criteria: Award Points"
 		
-			browser.find_element_by_xpath('//input[contains(@id, "00_ContentInfo_Results_SearchBox_SearchBy_rdosearchby3")]').click()
-			try:
-			    element = WebDriverWait(browser, 10).until(
-				EC.element_to_be_clickable((By.ID, "ctl00_ContentInfo_Results_SearchBox_btnSearch"))
-			    )
-			finally:
-			    None #driver.quit()
-
+			self.browser.find_element_by_xpath('//input[contains(@id, "00_ContentInfo_Results_SearchBox_SearchBy_rdosearchby3")]').click()
+			
 			print "criteria selected"
-			browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Results_SearchBox_btnSearch")]').click()	
+			self.browser.find_element_by_xpath('//input[contains(@id, "ctl00_ContentInfo_Results_SearchBox_btnSearch")]').click()	
 
-			dtls = browser.find_elements_by_xpath('//table[contains(@class, "rewardResults")]/tbody[2]/tr')					
+			dtls = self.browser.find_elements_by_xpath('//table[contains(@class, "rewardResults")]/tbody[2]/tr')					
 
 			for i in dtls:
 				try:
@@ -120,6 +129,8 @@ class getFares():
 			
 				self.a_flghtlist.append(tmp_list)
 		
+			# Compare the details obtained by both search criteria and filter out the common flight options
+			# Store the required details in a dictionary
 			n = 1
 			for i in range(0,len(self.p_flghtlist)):
 				for j in range(0,len(self.a_flghtlist)):
@@ -136,10 +147,11 @@ class getFares():
 
 		except NoSuchElementException:
 			print "Wrong Parameter Values"	
-		browser.close()
+
+		self.browser.close()
 		self.writeCSV()
 	
-	def writeCSV(self):
+	def writeCSV(self): # Function for writing data into results.csv file from result dictionary
 		with open('results.csv','w') as csvfile:
 			fieldnames = ['From','To','Depart Date','Arrive Date','Flight #','Time Depart','Time Arrive','Lowest Price','Award Points']
 			writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -148,10 +160,19 @@ class getFares():
 			for i in range(1,len(self.d_res.keys())):
 				writer.writerow({'From':self.src, 'To':self.dest, 'Depart Date':self.dptdt, 'Arrive Date':self.d_res[i][0], 'Flight #':self.d_res[i][1], 'Time Depart':self.d_res[i][2], 'Time Arrive':self.d_res[i][3], 'Lowest Price':self.d_res[i][4], 'Award Points':self.d_res[i][5]})
 		
-	
-myClassObject = getFares(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+def Start():
+	myClassObject = getFares(argv[1],argv[2],argv[3],argv[4])	
+	myClassObject.fillDtls()
 
-s = sched.scheduler(time.time, time.sleep)
-s.enter(int(sys.argv[5]), 1, myClassObject.fillDtls(), ())
-s.run()
+	
+if __name__ == "__main__":
+	if len(argv) != 6:
+    		print 'usage: %s <Source> <Destination> <Dept Date> <Return Date> <Interval>' %argv[0]
+    		exit(1)
+
+  	#myClassObject = getFares(argv[1],argv[2],argv[3],argv[4])
+	
+	s = sched.scheduler(time.time, time.sleep)
+	s.enter(int(argv[5]), 1, Start, ())
+	s.run()
 #myClassObject.fillDtls()
